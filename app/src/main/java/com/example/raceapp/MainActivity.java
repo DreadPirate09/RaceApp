6b65e188b935dc0850572f6f35290381f4599520
DreadPirate09 <62398759+DreadPirate09@users.noreply.github.com>
2021-06-04 16:33:35 +0300

    add fragment tabs
    add section of chat/race/people

--- app/src/main/java/com/example/raceapp/MainActivity.java
+++ app/src/main/java/com/example/raceapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.raceapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +20,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("We are in the on create function wtf romania!\n");
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        TabItem tabChats = findViewById(R.id.tabChat);
+        TabItem tabPeople = findViewById(R.id.tabPeople);
+        TabItem tabRace = findViewById(R.id.tabRace);
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(),
+                    tabLayout.getTabCount());
+
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager){
+            @Override
+            public void onTabSelected(@NonNull TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
     }
 
     public void logout(View view){
