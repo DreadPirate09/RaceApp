e162f1b8be81f5e577aa53e30999bb1c1398f9c1
DreadPirate09 <62398759+DreadPirate09@users.noreply.github.com>
2021-06-06 20:57:24 +0300

    news on register and others

--- app/src/main/java/com/example/raceapp/PeopleFragment.java
+++ app/src/main/java/com/example/raceapp/PeopleFragment.java
@@ -1,16 +1,36 @@
 package com.example.raceapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.raceapp.Adapter.UserAdapter;
+import com.example.raceapp.Model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PeopleFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+
+    private UserAdapter userAdapter;
+    private List<User> mUsers;
 
     public PeopleFragment() {
         // Required empty public constructor
@@ -19,7 +39,48 @@ public class PeopleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_people, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mUsers = new ArrayList<>();
+
+        readUsers();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_people, container, false);
+        return view;
+    }
+
+    private void readUsers(){
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        Log.d("GANGTAG","in readUsers\n "+reference);
+
+        reference.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d("GANGTAG","onDataChange\n");
+                mUsers.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+                    assert user != null;
+                    assert firebaseUser != null;
+                    if(!user.getId().equals(firebaseUser.getUid())){
+                        mUsers.add(user);
+                    }
+                }
+                userAdapter = new UserAdapter(getContext(), mUsers);
+                recyclerView.setAdapter(userAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/raceapp/Register.java
+++ app/src/main/java/com/example/raceapp/Register.java
@@ -2,10 +2,12 @@ package com.example.raceapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -18,18 +20,26 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class Register extends AppCompatActivity {
-    EditText mFullName,mEmail,mPassword,mPhone;
+    EditText mFullName,mEmail,mPassword;
     Button mRegisterBtn;
     TextView mLoginBtn;
     FirebaseAuth fAuth;
+    DatabaseReference reference;
     ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+//        Toolbar toolbar = findViewById(R.id.toolbar);
 
         mFullName = findViewById(R.id.personName);
         mEmail = findViewById(R.id.email);
@@ -38,47 +48,69 @@ public class Register extends AppCompatActivity {
         mLoginBtn = findViewById(R.id.goToLogin);
 
         fAuth = FirebaseAuth.getInstance();
-        progressBar = findViewById(R.id.progressBar);
 
         if(fAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(),MainActivity.class));
             finish();
         }
 
-        mRegisterBtn.setOnClickListener(v -> {
-            String email = mEmail.getText().toString().trim();
-            String password = mPassword.getText().toString().trim();
+        mRegisterBtn.setOnClickListener(new View.OnClickListener(){
 
-            if(TextUtils.isEmpty(email)){
-                mEmail.setError("Email is Required.");
-                return;
-            }
+            @Override
+            public void onClick(View v) {
+                String txt_username = mFullName.getText().toString().trim();
+                String txt_email = mEmail.getText().toString().trim();
+                String txt_password = mPassword.getText().toString().trim();
 
-            if(TextUtils.isEmpty(password)){
-                mPassword.setError("Password is Required.");
-                return;
-            }
-
-            if(password.length() < 6){
-                mPassword.setError("Password Must be >= 6 Characters");
-                return;
-            }
-
-            progressBar.setVisibility(View.VISIBLE);
-
-            fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()){
-                        Toast.makeText(Register.this, "User Created.", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                    }else{
-                        Toast.makeText(Register.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                    }
+                if(TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_email) || TextUtils.isEmpty((txt_password))){
+                    Toast.makeText(Register.this, "All fields are required",Toast.LENGTH_SHORT).show();
+                }else if (txt_password.length() < 6){
+                    Toast.makeText(Register.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+                }else{
+                    register(txt_username,txt_email,txt_password);
                 }
-            });
+            }
         });
 
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+
+
         mLoginBtn.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),Login.class)));
+    }
+
+    private void register(String username, String email, String password){
+        fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser firebaseUser = fAuth.getCurrentUser();
+                            assert firebaseUser != null;
+                            String userid = firebaseUser.getUid();
+
+                            reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("id", userid);
+                            hashMap.put("username",username);
+                            hashMap.put("imageURL","default");
+                            Log.d("REG:","Before to se the hashMap");
+                            reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Log.d("REG:","After adding the hashmap");
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+                        }else{
+                            Toast.makeText(Register.this, "You can't register with this email or password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
